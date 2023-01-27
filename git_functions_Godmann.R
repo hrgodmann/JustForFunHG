3f9266b2c0fb0f490beecfb0d87af3e693ca4e2b
hrgodmann <hrgodmann@gmail.com>
2023-01-27 20:24:42 +0100

    Update git_functions_Godmann.R

--- git_functions_Godmann.R
+++ git_functions_Godmann.R
@@ -1,5 +1,6 @@
 
 
+
 remind_me <- function(){
   cat("When you read this, you lost your memory and found a piece of paper\nthat told you inconspicuously that you wanted to run this function in R...")
   
