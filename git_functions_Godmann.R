fb01c58c19cc3959e1d8caf37f3e56cb68bdf0ab
hrgodmann <hrgodmann@gmail.com>
2023-01-27 20:18:10 +0100

    Create git_functions_Godmann.R

---
+++ git_functions_Godmann.R
@@ -0,0 +1,28 @@
+
+
+remind_me <- function(){
+  cat("When you read this, you lost your memory and found a piece of paper\nthat told you inconspicuously that you wanted to run this function in R...")
+  
+  cat("\n\nHere are things you wanted yourself to remind you of in the future:")
+  
+  cat("\n- always use Bayesian analyses")
+  cat("\n- ...end")
+}
+
+remind_me()
+
+
+cheat <- function(n){
+    if(n==1){
+    cat(
+    "dat <- rbeta(64, 10, 1.5)
+    dat <- round(dat * 10, 1)
+    hist(dat)")} else if(n==17){
+    cat("# On a Mac, it's Shift - Command - A', otherwise Ctrl + Shift + A")}else if(n==7){
+    cat('"ggplot(data = ChickWeight, aes(x = Time, y = weight)) + geom_smooth(method = "lm")"')
+    } else{message("Please ask for another assignment number")}
+}
+
+cheat(17)
+
+
