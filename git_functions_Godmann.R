28d73d25366e79b50c4e0d65907ca55d09e5b947
hrgodmann <hrgodmann@gmail.com>
2023-01-27 20:26:03 +0100

    Update git_functions_Godmann.R

--- git_functions_Godmann.R
+++ git_functions_Godmann.R
@@ -2,23 +2,23 @@
 
 
 remind_me <- function(){
-  cat("When you read this, you lost your memory and found a piece of paper\nthat told you inconspicuously that you wanted to run this function in R...")
+  print("When you read this, you lost your memory and found a piece of paper\nthat told you inconspicuously that you wanted to run this function in R...")
   
-  cat("\n\nHere are things you wanted yourself to remind you of in the future:")
+  print("\n\nHere are things you wanted yourself to remind you of in the future:")
   
-  cat("\n- always use Bayesian analyses")
-  cat("\n- ...end")
+  print("\n- always use Bayesian analyses")
+  print("\n- ...end")
 }
 
 
 cheat <- function(n){
     if(n==1){
-    cat(
+    print(
     "dat <- rbeta(64, 10, 1.5)
     dat <- round(dat * 10, 1)
     hist(dat)")} else if(n==17){
-    cat("# On a Mac, it's Shift - Command - A', otherwise Ctrl + Shift + A")}else if(n==7){
-    cat('"ggplot(data = ChickWeight, aes(x = Time, y = weight)) + geom_smooth(method = "lm")"')
+    print("# On a Mac, it's Shift - Command - A', otherwise Ctrl + Shift + A")}else if(n==7){
+    print('"ggplot(data = ChickWeight, aes(x = Time, y = weight)) + geom_smooth(method = "lm")"')
     } else{message("Please ask for another assignment number")}
 }
 
